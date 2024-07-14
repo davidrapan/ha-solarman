639b47fc3b1bb12c6b279a5fcf72a4f856eee72e
David Rapan <david@rapan.cz>
2024-07-14 17:34:12 +0200

    refactor: services.py constants moved to const.py

--- custom_components/solarman/services.py
+++ custom_components/solarman/services.py
@@ -1,14 +1,12 @@
+from __future__ import annotations
+
+import voluptuous as vol
+
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
-import voluptuous as vol
+
 from .const import *
 from .api import Inverter
-
-SERVICE_WRITE_REGISTER = "write_holding_register"
-SERVICE_WRITE_MULTIPLE_REGISTERS = "write_multiple_holding_registers"
-PARAM_REGISTER = "register"
-PARAM_VALUE = "value"
-PARAM_VALUES = "values"
 
 # Register the services one can invoke on the inverter.
 # Apart from this, it also need to be defined in the file 
@@ -16,29 +14,29 @@ PARAM_VALUES = "values"
 
 SERVICE_WRITE_REGISTER_SCHEMA = vol.Schema(
     {
-        vol.Required(PARAM_REGISTER): vol.All(vol.Coerce(int), vol.Range(min = 0, max = 65535)),
-        vol.Required(PARAM_VALUE): vol.All(vol.Coerce(int), vol.Range(min = 0, max = 65535)),
+        vol.Required(SERVICES_PARAM_REGISTER): vol.All(vol.Coerce(int), vol.Range(min = 0, max = 65535)),
+        vol.Required(SERVICES_PARAM_VALUE): vol.All(vol.Coerce(int), vol.Range(min = 0, max = 65535)),
     }
 )
 
 SERVICE_WRITE_MULTIPLE_REGISTERS_SCHEMA = vol.Schema(
     {
-        vol.Required(PARAM_REGISTER): vol.All(vol.Coerce(int), vol.Range(min = 0, max = 65535)),
-        vol.Required(PARAM_VALUES): vol.All(cv.ensure_list, [vol.All(vol.Coerce(int), vol.Range(min = 0, max = 65535))]),
+        vol.Required(SERVICES_PARAM_REGISTER): vol.All(vol.Coerce(int), vol.Range(min = 0, max = 65535)),
+        vol.Required(SERVICES_PARAM_VALUES): vol.All(cv.ensure_list, [vol.All(vol.Coerce(int), vol.Range(min = 0, max = 65535))]),
     }
 )
 
 def register_services(hass: HomeAssistant, inverter: Inverter):
     async def write_holding_register(call) -> None:
         await inverter.service_write_holding_register(
-            register = call.data.get(PARAM_REGISTER), 
-            value = call.data.get(PARAM_VALUE))
+            register = call.data.get(SERVICES_PARAM_REGISTER), 
+            value = call.data.get(SERVICES_PARAM_VALUE))
         return
 
     async def write_multiple_holding_registers(call) -> None:
         await inverter.service_write_multiple_holding_registers(
-            register = call.data.get(PARAM_REGISTER),
-            values = call.data.get(PARAM_VALUES))
+            register = call.data.get(SERVICES_PARAM_REGISTER),
+            values = call.data.get(SERVICES_PARAM_VALUES))
         return
 
     hass.services.async_register(
