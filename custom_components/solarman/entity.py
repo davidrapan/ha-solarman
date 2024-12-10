5d4c3374069355553b14c77882974311a981b3c6
David Rapan <david@rapan.cz>
2024-12-11 00:56:23 +0100

    fix: Set of configurables to 0 did not update state in HA

--- custom_components/solarman/entity.py
+++ custom_components/solarman/entity.py
@@ -147,7 +147,7 @@ class SolarmanWritableEntity(SolarmanEntity):
         if isinstance(value, list):
             while len(self.registers) > len(value):
                 value.insert(0, 0)
-        if await self.coordinator.inverter.call(self.code, self.register, value) > 0 and state:
+        if await self.coordinator.inverter.call(self.code, self.register, value) > 0 and state is not None:
             self.set_state(state, value)
             self.async_write_ha_state()
             #await self.entity_description.update_fn(self.coordinator., int(value))
