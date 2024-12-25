8bcc09da3ce681563a944d14e3fa3b94fcf0e893
David Rapan <david@rapan.cz>
2024-12-25 18:56:58 +0100

    refactor: Coordinator setup method

--- custom_components/solarman/coordinator.py
+++ custom_components/solarman/coordinator.py
@@ -14,25 +14,29 @@ _LOGGER = logging.getLogger(__name__)
 
 class InverterCoordinator(DataUpdateCoordinator[dict[str, Any]]):
     def __init__(self, hass: HomeAssistant, inverter: Inverter):
-        super().__init__(hass, _LOGGER, name = inverter.config.name, setup_method = inverter.load, update_interval = TIMINGS_UPDATE_INTERVAL, always_update = False)
+        super().__init__(hass, _LOGGER, name = inverter.config.name, update_interval = TIMINGS_UPDATE_INTERVAL, always_update = False)
         self.inverter = inverter
         self._counter = 0
 
-    def _accounting(self) -> int:
+    async def _async_setup(self) -> None:
         try:
-            return int(self._counter * self._update_interval_seconds)
-        finally:
-            if self.last_update_success:
-                self._counter += 1
+            return await self.inverter.load()
+        except Exception as e:
+            if isinstance(e, TimeoutError):
+                raise
+            raise UpdateFailed(e) from e
 
     async def _async_update_data(self) -> dict[str, Any]:
         try:
-            return await self.inverter.get(self._accounting())
+            return await self.inverter.get(int(self._counter * self._update_interval_seconds))
         except Exception as e:
             self._counter = 0
             if isinstance(e, TimeoutError):
                 raise
             raise UpdateFailed(e) from e
+        finally:
+            if self.last_update_success:
+                self._counter += 1
 
     #async def _reload(self):
     #    _LOGGER.debug('_reload')
