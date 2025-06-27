247ddc33b10d9348addb1363198a743737b9eb84
David Rapan <david@rapan.cz>
2025-06-28 01:44:58 +0200

    refactor: Make diagnostics dump to remove all sensitive information
    Signed-off-by: David Rapan <david@rapan.cz>

--- custom_components/solarman/diagnostics.py
+++ custom_components/solarman/diagnostics.py
@@ -1,20 +1,16 @@
-from __future__ import annotations
-
-from typing import Any
-
 from homeassistant.core import HomeAssistant
 from homeassistant.components.diagnostics import async_redact_data
 
 from . import SolarmanConfigEntry
 
-TO_REDACT = {"mac", "serial_number"}
+TO_REDACT = {"identifiers", "connections", "serial_number", "mac", "device_serial_number_sensor"}
 
-async def async_get_config_entry_diagnostics(_: HomeAssistant, config_entry: SolarmanConfigEntry) -> dict[str, Any]:
+async def async_get_config_entry_diagnostics(_: HomeAssistant, config_entry: SolarmanConfigEntry):
     return {
         "entry": {
             "title": config_entry.title,
             "config": async_redact_data(config_entry, TO_REDACT),
             "device_info": async_redact_data(config_entry.runtime_data.device.info, TO_REDACT),
         },
-        "data": config_entry.runtime_data.data
+        "data": async_redact_data(config_entry.runtime_data.data)
     }
