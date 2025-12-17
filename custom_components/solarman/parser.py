6129db0cd8895e7c21ffd3278c583e37bf6da406
Sopsy <721951+Sopsy@users.noreply.github.com>
2025-12-18 00:34:08 +0200

    Reversing as a rule 5 parameter

--- custom_components/solarman/parser.py
+++ custom_components/solarman/parser.py
@@ -171,8 +171,6 @@ class ParameterParser:
                                 self.try_parse_time(data, i)
                             case 10:
                                 self.try_parse_raw(data, i)
-                            case 11:
-                                self.try_parse_ascii_reverse(data, i)
                     except Exception as e:
                         _LOGGER.error(f"ParameterParser.try_parse: data: {data}, definition: {i} [{strepr(e)}]")
                         raise
@@ -334,20 +332,10 @@ class ParameterParser:
             if (temp := get_addr_value(data, code, r)) is None:
                 return
 
-            value += chr(temp >> 8) + chr(temp & 0xFF)
-
-        self.set_state(definition["key"], value)
-
-    def try_parse_ascii_reverse(self, data, definition):
-        code = get_code(definition, "read")
-        value = ""
-
-        for r in definition["registers"]:
-            if (temp := get_addr_value(data, code, r)) is None:
-                return
-
             temp = chr(temp >> 8) + chr(temp & 0xFF)
-            temp = temp[::-1]
+            if "reverse_bytes" in definition:
+                temp = temp[::-1]
+
             value += temp
 
         self.set_state(definition["key"], value)
